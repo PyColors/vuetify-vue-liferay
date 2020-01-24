59b7a93e9ebfe19143d25121ba8bd3381ef9e328
Patrice <patweb44@gmail.com>
2020-01-24 22:21:45 +0100

    Adding packages and config files

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
-}
+  outputDir: "build/",
+  publicPath: "/o/vuetify-vue-portlet/",
+  pages: {
+    index: {
+      entry: ".webpack/index.js",
+      template: ".webpack/index.html"
+    }
+  },
+  configureWebpack: {
+    performance: {
+      hints: false
+    }
+  },
+  transpileDependencies: ["vuetify"]
+};
